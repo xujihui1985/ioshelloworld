74362d4468d168d39e6e1715796d6ca2ff4de2cc
Sean <xujihui1985@gmail.com>
2014-04-16 22:14:51 +0800

    Initial Commit

---
+++ ProteinTracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProteinTracker
+//
+//  Created by Sean on 4/16/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ProteinTrackerTests/ProteinTrackerTests.m
@@ -0,0 +1,34 @@
+//
+//  ProteinTrackerTests.m
+//  ProteinTrackerTests
+//
+//  Created by Sean on 4/16/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProteinTrackerTests : XCTestCase
+
+@end
+
+@implementation ProteinTrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
